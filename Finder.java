afb68034def0f836b69dad424e131c843520b88f
Joseda8 <jd.montoya.s8@gmail.com>
2017-05-15 18:14:43 -0600

    Add files via upload

---
+++ Finder.java
@@ -0,0 +1,274 @@
+package com.meditec.medmanagement;
+
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+import com.meditec.clientmanagement.Patient;
+import com.meditec.datastructures.AVLNode;
+import com.meditec.datastructures.AVLTree;
+import com.meditec.datastructures.BinaryTree;
+import com.meditec.datastructures.SplayNode;
+import com.meditec.datastructures.SplayTree;
+import com.meditec.datastructures.TreeNode;
+import com.meditec.resources.Chat;
+import com.meditec.resources.MedicResources;
+import com.meditec.resources.PatientResources;
+
+public class Finder {
+	
+	public static JSONObject get_all_medics(){
+		return get_all_medics(MedicResources.medic_tree.root(), new JSONObject(), 1);
+	}
+	
+	private static JSONObject get_all_medics(SplayNode<Medic> node, JSONObject json, int count){
+		if (node != null) {
+			get_all_medics(node.left, json, count + 1);
+			json.put(String.valueOf(count), node.element.code());
+			get_all_medics(node.right, json, count + 1);
+		}
+		json.put("count", MedicResources.medic_tree.countNodes());
+		return json;
+	}
+
+	public static Medic find_medic_by_name(String name){
+        return find_medic_by_name(MedicResources.medic_tree.root(), name).element;
+    }
+
+    private static SplayNode<Medic> find_medic_by_name(SplayNode<Medic> node, String name){
+        if (node != null){
+            if (node.element.name().equals(name)){
+                return node;
+            }else {
+                SplayNode<Medic> temp = find_medic_by_name(node.left, name);
+                if (temp == null){
+                    temp = find_medic_by_name(node.right,name);
+                }
+                return temp;
+            }
+        }else {
+            return null;
+        }
+    }
+
+    public static Medic find_medic_by_code(String code){
+        return find_medic_by_code(MedicResources.medic_tree.root(),code).element;
+    }
+
+    private static SplayNode<Medic> find_medic_by_code(SplayNode<Medic> node, String code) {
+
+        if (node != null){
+            if (node.element.code().equals(code)){
+                return node;
+            }else {
+                SplayNode<Medic> temp = find_medic_by_code(node.left, code);
+                if (temp == null){
+                    temp = find_medic_by_code(node.right,code);
+                }
+                return temp;
+            }
+        }else {
+            return null;
+        }
+    }
+	
+	public static Patient find_patient(String name){
+		return find_patient(PatientResources.patients_tree.root(), name).data();
+	}
+	
+	private static 	AVLNode<Patient> find_patient(AVLNode<Patient> node, String name){
+		
+		 if (node != null){
+	            if (node.data().name().equals(name)){
+	                return node;
+	            }else {
+	                AVLNode<Patient> temp = find_patient(node.left(), name);
+	                if (temp == null){
+	                    temp = find_patient(node.right(),name);
+	                }
+	                return temp;
+	            }
+	        }else {
+	            return null;
+	        }
+	}
+	
+	public static JSONObject get_medic_appointments(String id){
+		Medic medic = find_medic_by_code(id);
+		return get_medic_appointments(medic.agenda().appointments().root(), new JSONObject(), 1, medic);
+	}
+	
+	private static JSONObject get_medic_appointments(AVLNode<Appointment> node, JSONObject json, int count, Medic medic){
+		if (node != null) {
+			get_medic_appointments(node.left(), json, count + 1,medic);
+			json.put(String.valueOf(count), node.data().patient());
+			get_medic_appointments(node.right(), json, count + 1,medic);
+		}
+		json.put("count", count);
+		return json;
+	}
+	
+	public static Appointment get_appointment(String name, AVLTree<Appointment> tree){
+		return get_appointment(tree.root(), name).data();
+	}
+	
+	private static AVLNode<Appointment> get_appointment(AVLNode<Appointment> node, String name){
+		if (node != null) {
+			if (node.data().patient().equals(name)) {
+				return node;
+			}else{
+				AVLNode<Appointment> tmp = get_appointment(node.left(), name);
+				if (tmp == null) {
+					tmp = get_appointment(node.right(), name);
+				}
+				return tmp;
+			}
+		}else {
+			return null;
+		}
+	}
+	
+	public static ClinicCase find_case(String name){
+		return find_case(name.toLowerCase(), MedicResources.cases.root()).data();
+	}
+	
+	private static TreeNode<ClinicCase> find_case(String name, TreeNode<ClinicCase> node){
+		if (node != null) {
+			if (node.data().name().toLowerCase().equals(name)) {
+				return node;
+			}else {
+				TreeNode<ClinicCase> tmp = find_case(name, node.get_left());
+				if (tmp == null) {
+					tmp = find_case(name, node.get_right());
+				}
+				return tmp;
+			}
+		}else {
+			return null;
+		}
+	}
+	
+	public static String get_medication_list(AVLTree<Medication> tree){
+		return get_medication_list(tree.root(), "");
+	}
+	
+	private static String get_medication_list(AVLNode<Medication> node, String result){
+		if (node == null) {
+			return "";
+		}
+		result += get_medication_list(node.left(), result);
+		result += get_medication_list(node.right(), result);
+		result += node.data().name() + ",";
+		return result;
+	}
+	
+	public static String get_tests_list(SplayTree<MedicTest> tree){
+		return get_tests_list(tree.root(), "");
+	}
+	
+	private static String get_tests_list(SplayNode<MedicTest> node, String result){
+		if (node == null) {
+			return "";
+		}
+		result += get_tests_list(node.left, result);
+		result += get_tests_list(node.right,result);
+		result += node.element.name() + ",";
+		return result;
+	}
+	
+	public static JSONObject get_all_cases(BinaryTree<ClinicCase> tree){
+		return get_all_cases(tree.root(), new JSONObject(), 1);
+	}
+	
+	private static JSONObject get_all_cases(TreeNode<ClinicCase> node, JSONObject json, int count){
+		if (node != null) {
+			System.out.println(node.data().name());
+			get_all_cases(node.get_left(), json , count + 1);
+			json.put(String.valueOf(count), node.data().name());
+			json.put("count", count);
+			get_all_cases(node.get_right(), json, count + 1);
+		}
+		return json;
+	}
+	
+	public static Medication find_medication(String name, AVLTree<Medication> tree){
+		return find_medication(name, tree.root()).data();
+	}
+	
+	private static AVLNode<Medication> find_medication(String name, AVLNode<Medication> node){
+		if (node != null) {
+			if (node.data().name().toLowerCase().equals(name.toLowerCase())) {
+				return node;
+			}else {
+				AVLNode<Medication> tmp = find_medication(name, node.left());
+				if (tmp == null) {
+					tmp = find_medication(name, node.right());
+				}
+				return tmp;
+			}
+		}else {
+			return null;
+		}
+	}
+	
+	public static MedicTest find_test(String name, SplayTree<MedicTest> tree){
+		return find_test(name, tree.root()).element;
+	}
+	
+	private static SplayNode<MedicTest> find_test(String name, SplayNode<MedicTest> node){
+		if (node != null) {
+			if (node.element.name().toLowerCase().equals(name.toLowerCase())) {
+				return node;
+			}else {
+				SplayNode<MedicTest> tmp = find_test(name, node.left);
+				if (tmp == null) {
+					tmp = find_test(name, node.right);
+				}
+				return tmp;
+			}
+		}else {
+			return null;
+		}
+	}
+	
+	public static JSONObject get_all_tests(SplayTree<MedicTest> tree){
+		return get_all_tests(tree.root(), new JSONObject(), 1);
+	}
+	
+	private static JSONObject get_all_tests(SplayNode<MedicTest> node, JSONObject json, int count){
+		if (node != null) {
+			get_all_tests(node.left, json , count + 1);
+			json.put(String.valueOf(count), node.element.name());
+			json.put("count", count);
+			get_all_tests(node.right, json, count + 1);
+		}
+		return json;
+	}
+	
+	public static JSONObject get_all_medication(AVLTree<Medication> tree){
+		return get_all_medication(tree.root(), new JSONObject(), 1);
+	}
+	
+	private static JSONObject get_all_medication(AVLNode<Medication> node, JSONObject json, int count){
+		if (node != null) {
+			get_all_medication(node.left(), json , count + 1);
+			json.put(String.valueOf(count), node.data().name());
+			get_all_medication(node.right(), json, count + 1);
+		}
+		json.put("count", MedicResources.medication.count());
+		return json;
+	}
+	
+	public static JSONObject get_chat_msg(){
+		return get_chat_msg(Chat.getList(), new JSONObject());
+	}
+	
+	private static JSONObject get_chat_msg(ArrayList<String> list, JSONObject json){
+		for(int i=0; i<list.size(); i++) {
+			  System.out.println(list.get(i));
+			  json.put(String.valueOf(i), list.get(i));
+			} 
+	
+		json.put("count", list.size());
+		return json;
+	}
+}
---
+++ MedicResources.java
@@ -0,0 +1,260 @@
+package com.meditec.resources;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.ResponseBuilder;
+
+import org.json.JSONObject;
+
+import com.meditec.datastructures.AVLTree;
+import com.meditec.datastructures.BinaryTree;
+import com.meditec.datastructures.SplayTree;
+import com.meditec.medmanagement.Medic;
+import com.meditec.medmanagement.MedicTest;
+import com.meditec.medmanagement.Medication;
+import com.meditec.utilities.IdentifiersGenerator;
+import com.meditec.utilities.JSONHandler;
+import com.meditec.utilities.XMLHandler;
+import com.meditec.medmanagement.Appointment;
+import com.meditec.medmanagement.ClinicCase;
+import com.meditec.medmanagement.Finder;
+
+@Path("/medics")
+public class MedicResources {
+	
+	public static SplayTree<Medic> medic_tree = new SplayTree<>();
+	public static BinaryTree<ClinicCase> cases = new BinaryTree<>();
+	public static SplayTree<MedicTest> tests = new SplayTree<>();
+	public static AVLTree<Medication> medication = new AVLTree<>();
+	
+	@POST
+	@Path("/login")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response log_medic(String json_info){
+		
+		JSONObject medic_json = new JSONObject(json_info);
+		
+		System.out.println(medic_json.getString("name"));
+		
+		try{
+			Medic m = Finder.find_medic_by_name(medic_json.getString("name"));
+		}catch (NullPointerException e) {
+			Medic new_medic = new Medic(medic_json.getString("name"), medic_json.getString("email"));
+			create_dummy_medics();
+			process_medic(new_medic);
+			XMLHandler.add_cases_to_tree(cases);
+			XMLHandler.add_tests_to_tree(tests);
+			XMLHandler.add_medication_to_tree(medication);
+			return Response.ok(JSONHandler.get_identifier(new_medic.code())).build();
+		}
+		
+		Medic medic = Finder.find_medic_by_name(medic_json.getString("name"));
+		
+		return Response.ok(JSONHandler.get_identifier(medic.code())).build();
+		
+	}
+	
+	@POST
+	@Path("/chat")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response msg_chat(String json_info){
+		
+		JSONObject msg_chat = new JSONObject(json_info);
+		String msg = msg_chat.getString("msg");
+		System.out.println(msg_chat.getString("msg"));
+		
+        Chat.getList().add(msg.toString());
+		return Response.ok(msg).build();	
+	}
+	
+	@GET
+	@Path("/chat_list")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_chat_list(){
+		return Response.ok(Finder.get_chat_msg().toString()).build();
+	}
+	
+	@GET
+	@Path("/{id}/appointments")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_medic_appointments(@PathParam("id") String id){
+		JSONObject appointments = Finder.get_medic_appointments(id);
+		return Response.ok(appointments.toString()).build();
+	}
+	
+	@GET
+	@Path("/{id}/appointments/{patient}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_client_appointment(@PathParam("id") String id, @PathParam("patient") String patient_name){
+		Medic medic = Finder.find_medic_by_code(id);
+		Appointment appointment = medic.agenda().get_appointment_info(patient_name);
+		return Response.ok(JSONHandler.get_json_appointment(appointment)).build();
+	}
+	
+	@PUT
+	@Path("/{id}/appointments/{patient}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response edit_appointment_info(String updated_info, @PathParam("id") String id, @PathParam("patient") String patient){
+		System.out.println(updated_info);
+		Medic medic = Finder.find_medic_by_code(id);
+		Appointment appointment = medic.agenda().get_appointment_info(patient);
+		medic.agenda().edit_appointment(JSONHandler.get_case_name(updated_info), JSONHandler.get_updated_symptoms(updated_info), JSONHandler.get_updated_medication(updated_info), JSONHandler.get_updated_tests(updated_info), JSONHandler.get_updated_cases(updated_info), appointment);
+		return Response.ok("Appointment edited!").build();
+	}
+	
+	@POST
+	@Path("/cases/new_case")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create_clinic_case(String case_info){
+		System.out.println(case_info);
+		ClinicCase new_case = JSONHandler.parse_new_clinic_case(case_info);
+		cases.insert(new_case.key(), new_case);
+		return Response.ok("Clinic case succesfully created").build();
+	}
+	
+	@GET
+	@Path("/cases")
+	public Response get_cases_list() {
+		return Response.ok(Finder.get_all_cases(cases).toString()).build();
+	}
+	
+	@DELETE
+	@Path("/cases/{name}")
+	public Response delete_clinic_case(@PathParam("name") String case_name) {
+		cases.remove(Finder.find_case(case_name).key());
+		return Response.ok("Case removed successfully").build();
+	}
+	
+	@GET
+	@Path("/cases/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_case_details(@PathParam("name") String name) {
+		return Response.ok(JSONHandler.parse_clinic_case(Finder.find_case(name))).build();
+	}
+	
+	@PUT
+	@Path("/cases/{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response edit_case_details(String json_details, @PathParam("name") String case_name){
+		ClinicCase clinic_case = Finder.find_case(case_name);
+		JSONObject details = new JSONObject(json_details);
+		clinic_case.edit_case(details.getString("tests"), details.getString("medication"));
+		return Response.ok("Clinic Case Edited!").build();
+	}
+	
+	@GET
+	@Path("/tests")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_all_tests(){
+		return Response.ok(Finder.get_all_tests(tests).toString()).build();
+	}
+	
+	@POST
+	@Path("/tests/new_test")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create_new_test(String new_test){
+		MedicTest medic_test = JSONHandler.build_new_test(new_test);
+		tests.insert(medic_test, medic_test.id());
+		return Response.ok("Test created succesfully").build();
+	}
+	
+	@DELETE
+	@Path("/tests/{name}")
+	public Response delete_test(@PathParam("name") String name){
+		tests.remove(Finder.find_test(name, tests).id());
+		return Response.ok("Test " + name + " eliminated succesfully").build();
+		
+	}
+	
+	@PUT
+	@Path("/tests/{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response edit_test(String new_info, @PathParam("name") String name){
+		MedicTest m = Finder.find_test(name, tests);
+		JSONObject json_test = JSONHandler.parse(new_info);
+		m.edit_name(json_test.getString("name"));
+		m.edit_price(json_test.getInt("cost"));
+		return Response.ok("Test updated!").build();
+	}
+	
+	@GET
+	@Path("/tests/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_test_details(@PathParam("name") String name){
+		return Response.ok(JSONHandler.build_test_details(Finder.find_test(name, tests))).build();
+	}
+	
+	@GET
+	@Path("/medication")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_all_medication(){
+		return Response.ok(Finder.get_all_medication(medication).toString()).build();
+	}
+	
+	@DELETE
+	@Path("/medication/{name}")
+	public Response delete_medication(@PathParam("name") String name){
+		medication.remove(Finder.find_medication(name, medication));
+		return Response.ok("Medication removed!").build();
+	}
+	
+	@PUT
+	@Path("/medication/{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response edit_medication(String info, @PathParam("name") String name){
+		Medication m = Finder.find_medication(name, medication);
+		JSONObject json_test = JSONHandler.parse(info);
+		m.edit_name(json_test.getString("name"));
+		m.edit_cost(json_test.getInt("cost"));
+		return Response.ok("Medication updated!").build();
+	}
+	
+	@GET
+	@Path("/medication/{name}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get_medication_details(@PathParam("name") String name){
+		return Response.ok(JSONHandler.build_medication_details(Finder.find_medication(name, medication))).build();
+	}
+		
+	@POST
+	@Path("/medication/new_medication")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response create_medication(String new_medication){
+		Medication medication = JSONHandler.build_new_medication(new_medication);
+		this.medication.insert(medication);
+		return Response.ok(medication.name() + " created!").build();		
+	}
+	
+
+	
+	private void process_medic(Medic medic){
+		medic_tree.insert(medic, IdentifiersGenerator.generate_new_key(3));
+	}
+	
+	
+	
+	private void create_dummy_medics(){
+		
+		Medic a = new Medic("Alonso", "some@email.com");
+		Medic b = new Medic("Bryan", "some@email.com");
+		Medic c = new Medic("Jennifer", "some@email.com");
+		Medic d = new Medic("Fabian", "some@email.com");
+		Medic e = new Medic("Kate", "some@email.com");
+		
+		medic_tree.insert(a, IdentifiersGenerator.generate_new_key(3));
+		medic_tree.insert(b, IdentifiersGenerator.generate_new_key(3));
+		medic_tree.insert(c, IdentifiersGenerator.generate_new_key(3));
+		medic_tree.insert(d, IdentifiersGenerator.generate_new_key(3));
+		medic_tree.insert(e, IdentifiersGenerator.generate_new_key(3));
+	}
+	
+
+}
